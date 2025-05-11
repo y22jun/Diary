4ed48d4f7728f7eeac63df3a72521211b326de60
y22jun <tlsdpwns0612@naver.com>
2025-05-11 16:58:59 +0900

    Chore: 안 쓰는 import 제거

--- src/main/java/org/zeorck/diary/domain/diary/application/DiaryService.java
+++ src/main/java/org/zeorck/diary/domain/diary/application/DiaryService.java
@@ -2,7 +2,6 @@ package org.zeorck.diary.domain.diary.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
